60b6c7ab7efd575644dfef1f20ec718c94fde931
Amlet <76486163+Amlet04@users.noreply.github.com>
2023-10-16 12:45:05 +0200

    Update confirm_link.py

--- src/updates/callbacks/admin/confirm_link.py
+++ src/updates/callbacks/admin/confirm_link.py
@@ -1,1 +1,25 @@
+from ....objects.user import User
+from pony.orm import db_session, delete
 
+def process_callback(message, data, query):
+    u = User(query.sender)
+    u.state("home")
+    
+    if data:
+        u.set_affiliazione({
+            "active": True
+        })
+    
+        text = (
+            "<b>Nuova affiliazione aggiunta con successo ✅</b>"
+        )
+
+    else:
+        u.get_affiliazione(last=True, delete=True)
+        
+        text = (
+            "<b>Affiliazione Eliminata.</b>\n"
+            "Digita /addlink per aggiungerne una nuova."
+        )
+    
+    message.edit(text, syntax="HTML")
