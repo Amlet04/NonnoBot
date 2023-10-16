7ecc736fed57e4611828515a554632e4fa50239c
Amlet <76486163+Amlet04@users.noreply.github.com>
2023-10-16 12:51:53 +0200

    Create __init__.py

---
+++ src/updates/commands/__init__.py
@@ -0,0 +1,5 @@
+from .start import process_command
+from .admin.add_link import process_command
+from .admin.cancel import process_command
+from .admin.authorize_user import process_command
+from .admin.unauthorize_user import process_command
